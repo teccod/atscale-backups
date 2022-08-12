7f4dea61f4a36b436b91d4bd87dc16d67a66a7f6
NjekTt <v.berkutovv@gmail.com>
2022-08-12 13:59:37 +0400

    Fix file structure

--- AtScaleBackups/AtScaleBackupsSetup.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-def setup():
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "configparser"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
